trefis/ocaml

patterns: reuse ('a Typedtree.pattern_) to define Patterns.Head.t

typing/patterns.ml
@@ -30,12 +30,7 @@ module Head : sig
     | Array of int
     | Lazy
 
-  type t
-
-  val desc : t -> desc
-  val env : t -> Env.t
-  val loc : t -> Location.t
-  val typ : t -> Types.type_expr
+  type t = desc pattern_
 
   val arity : t -> int
 
@@ -47,15 +42,7 @@ module Head : sig
   (** reconstructs a pattern, putting wildcards as sub-patterns. *)
   val to_omega_pattern : t -> pattern
 
-  val make
-    :  loc:Location.t
-    -> typ:Types.type_expr
-    -> env:Env.t
-    -> desc
-    -> t
-
   val omega : t
-
 end = struct
   type desc =
     | Any
@@ -72,18 +59,7 @@ end = struct
     | Array of int
     | Lazy
 
-  type t = {
-    desc: desc;
-    typ : Types.type_expr;
-    loc : Location.t;
-    env : Env.t;
-    attributes : attributes;
-  }
-
-  let desc { desc } = desc
-  let env { env } = env
-  let loc { loc } = loc
-  let typ { typ } = typ
+  type t = desc pattern_
 
   let deconstruct q =
     let rec deconstruct_desc = function
@@ -118,11 +94,10 @@ end = struct
       | Tpat_or _ -> invalid_arg "Parmatch.Pattern_head.deconstruct: (P | Q)"
     in
     let desc, pats = deconstruct_desc q.pat_desc in
-    { desc; typ = q.pat_type; loc = q.pat_loc;
-      env = q.pat_env; attributes = q.pat_attributes }, pats
+    { q with pat_desc = desc }, pats
 
   let arity t =
-    match t.desc with
+    match t.pat_desc with
       | Any -> 0
       | Constant _ -> 0
       | Construct c -> c.cstr_arity
@@ -133,14 +108,15 @@ end = struct
 
   let to_omega_pattern t =
     let pat_desc =
-      match t.desc with
+      let mkloc x = Location.mkloc x t.pat_loc in
+      match t.pat_desc with
       | Any -> Tpat_any
       | Lazy -> Tpat_lazy omega
       | Constant c -> Tpat_constant c
       | Tuple n -> Tpat_tuple (omegas n)
       | Array n -> Tpat_array (omegas n)
       | Construct c ->
-          let lid_loc = Location.mkloc (Longident.Lident c.cstr_name) t.loc in
+          let lid_loc = mkloc (Longident.Lident c.cstr_name) in
           Tpat_construct (lid_loc, c, omegas c.cstr_arity)
       | Variant { tag; has_arg; cstr_row } ->
           let arg_opt = if has_arg then Some omega else None in
@@ -148,25 +124,16 @@ end = struct
       | Record lbls ->
           let lst =
             List.map (fun lbl ->
-              let lid_loc =
-                Location.mkloc (Longident.Lident lbl.lbl_name) t.loc
-              in
+              let lid_loc = mkloc (Longident.Lident lbl.lbl_name) in
               (lid_loc, lbl, omega)
             ) lbls
           in
           Tpat_record (lst, Closed)
     in
-    { pat_desc; pat_type = t.typ; pat_loc = t.loc; pat_extra = [];
-      pat_env = t.env; pat_attributes = t.attributes }
-
-  let make ~loc ~typ ~env desc =
-    { desc; loc; typ; env; attributes = [] }
-
-  let omega =
-    { desc = Any
-    ; loc = Location.none
-    ; typ = Ctype.none
-    ; env = Env.empty
-    ; attributes = []
+    { t with
+      pat_desc;
+      pat_extra = [];
     }
+
+  let omega = { omega with pat_desc = Any }
 end

typing/patterns.mli
@@ -27,12 +27,7 @@ module Head : sig
     | Array of int
     | Lazy
 
-  type t
-
-  val desc : t -> desc
-  val env : t -> Env.t
-  val loc : t -> Location.t
-  val typ : t -> Types.type_expr
+  type t = desc pattern_
 
   val arity : t -> int
 
@@ -43,13 +38,6 @@ module Head : sig
 
   (** reconstructs a pattern, putting wildcards as sub-patterns. *)
   val to_omega_pattern : t -> pattern
-
-  val make
-    :  loc:Location.t
-    -> typ:Types.type_expr
-    -> env:Env.t
-    -> desc
-    -> t
 
   val omega : t
 

andreypopp/melange-compiler-libs

typing/patterns.ml
@@ -30,12 +30,7 @@ module Head : sig
     | Array of int
     | Lazy
 
-  type t
-
-  val desc : t -> desc
-  val env : t -> Env.t
-  val loc : t -> Location.t
-  val typ : t -> Types.type_expr
+  type t = desc pattern_data
 
   val arity : t -> int
 
@@ -47,15 +42,7 @@ module Head : sig
   (** reconstructs a pattern, putting wildcards as sub-patterns. *)
   val to_omega_pattern : t -> pattern
 
-  val make
-    :  loc:Location.t
-    -> typ:Types.type_expr
-    -> env:Env.t
-    -> desc
-    -> t
-
   val omega : t
-
 end = struct
   type desc =
     | Any
@@ -72,18 +59,7 @@ end = struct
     | Array of int
     | Lazy
 
-  type t = {
-    desc: desc;
-    typ : Types.type_expr;
-    loc : Location.t;
-    env : Env.t;
-    attributes : attributes;
-  }
-
-  let desc { desc } = desc
-  let env { env } = env
-  let loc { loc } = loc
-  let typ { typ } = typ
+  type t = desc pattern_data
 
   let deconstruct q =
     let rec deconstruct_desc = function
@@ -118,11 +94,10 @@ end = struct
       | Tpat_or _ -> invalid_arg "Parmatch.Pattern_head.deconstruct: (P | Q)"
     in
     let desc, pats = deconstruct_desc q.pat_desc in
-    { desc; typ = q.pat_type; loc = q.pat_loc;
-      env = q.pat_env; attributes = q.pat_attributes }, pats
+    { q with pat_desc = desc }, pats
 
   let arity t =
-    match t.desc with
+    match t.pat_desc with
       | Any -> 0
       | Constant _ -> 0
       | Construct c -> c.cstr_arity
@@ -133,14 +108,15 @@ end = struct
 
   let to_omega_pattern t =
     let pat_desc =
-      match t.desc with
+      let mkloc x = Location.mkloc x t.pat_loc in
+      match t.pat_desc with
       | Any -> Tpat_any
       | Lazy -> Tpat_lazy omega
       | Constant c -> Tpat_constant c
       | Tuple n -> Tpat_tuple (omegas n)
       | Array n -> Tpat_array (omegas n)
       | Construct c ->
-          let lid_loc = Location.mkloc (Longident.Lident c.cstr_name) t.loc in
+          let lid_loc = mkloc (Longident.Lident c.cstr_name) in
           Tpat_construct (lid_loc, c, omegas c.cstr_arity)
       | Variant { tag; has_arg; cstr_row } ->
           let arg_opt = if has_arg then Some omega else None in
@@ -148,25 +124,16 @@ end = struct
       | Record lbls ->
           let lst =
             List.map (fun lbl ->
-              let lid_loc =
-                Location.mkloc (Longident.Lident lbl.lbl_name) t.loc
-              in
+              let lid_loc = mkloc (Longident.Lident lbl.lbl_name) in
               (lid_loc, lbl, omega)
             ) lbls
           in
           Tpat_record (lst, Closed)
     in
-    { pat_desc; pat_type = t.typ; pat_loc = t.loc; pat_extra = [];
-      pat_env = t.env; pat_attributes = t.attributes }
-
-  let make ~loc ~typ ~env desc =
-    { desc; loc; typ; env; attributes = [] }
-
-  let omega =
-    { desc = Any
-    ; loc = Location.none
-    ; typ = Ctype.none
-    ; env = Env.empty
-    ; attributes = []
+    { t with
+      pat_desc;
+      pat_extra = [];
     }
+
+  let omega = { omega with pat_desc = Any }
 end

typing/patterns.mli
@@ -27,12 +27,7 @@ module Head : sig
     | Array of int
     | Lazy
 
-  type t
-
-  val desc : t -> desc
-  val env : t -> Env.t
-  val loc : t -> Location.t
-  val typ : t -> Types.type_expr
+  type t = desc pattern_data
 
   val arity : t -> int
 
@@ -43,13 +38,6 @@ module Head : sig
 
   (** reconstructs a pattern, putting wildcards as sub-patterns. *)
   val to_omega_pattern : t -> pattern
-
-  val make
-    :  loc:Location.t
-    -> typ:Types.type_expr
-    -> env:Env.t
-    -> desc
-    -> t
 
   val omega : t
 